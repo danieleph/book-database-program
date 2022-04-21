901dd38b53ca05258e086a0b6b1ad275a927c5a2
danieleph <daniel.stevens@ephcreative.co.uk>
2022-04-21 15:19:25 +0100

    able to view a book

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def clean_date(date_str):
 
     #datetime.date()
 
-def clean_price(price_str):
+def clean_price(price_str, options):
     try:
         price_float = float(price_str)
     except ValueError:
@@ -73,6 +73,27 @@ def clean_price(price_str):
     else:
         return int(price_float * 100)
     #print(price_float)
+
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input('''
+            \n****** ID ERROR *****
+            \rThe id should be a number.
+            \rPlease enter to try again.
+            \r*********************''')
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f'''
+                \n****** ID ERROR *****
+                \rOptions: {options}
+                \rPlease enter to try again.
+                \r*********************''')
+            return
 
 
 def add_csv():
@@ -126,7 +147,23 @@ def app():
             input('\nPress enter to return to main menu.')
         elif choice == '3':
             # search book
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f'''
+                    \nID Options: {id_options}
+                    \rBook id: ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+                \n{the_book.title} by {the_book.author}
+                \rPublished: {the_book.published_date}
+                \rPrice: ${the_book.price / 100}''')
+            input('\nPress enter to return to the main menu')
         elif choice == '4':
             #book analysis
             pass
