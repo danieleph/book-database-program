83d13d7e91c0eaeaba0604cf792901e20336838e
danieleph <daniel.stevens@ephcreative.co.uk>
2022-04-21 10:22:07 +0100

    Actually adding database model

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from models import (Base, session,
+                    Book, engine)
+
+
+
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to the database
@@ -5,3 +10,7 @@
 # search books
 # data cleaning
 # loop runs program
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
