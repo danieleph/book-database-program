986d3f607f65979a42a4014942ebf377a2a5fb74
danieleph <daniel.stevens@ephcreative.co.uk>
2022-04-22 08:48:20 +0100

    3. Added search for a book -  sub menu edit and delete functions

--- app.py
+++ app.py
@@ -23,6 +23,21 @@ def menu():
                 \rA number from 1 to 5.
                 \rPress enter to try again.''')
 
+def submenu():
+    while True:
+        print('''
+                \nPROGRAMMING BOOKS
+                \r1) Edit
+                \r2) Delete
+                \r3) Return to main menu''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3']:
+            return choice
+        else:
+            input('''
+                \rPlease choose one of the options above.
+                \rA number from 1 to 3.
+                \rPress enter to try again.''')
 
 
 
@@ -34,6 +49,9 @@ def menu():
 # search books
 # data cleaning
 # loop runs program
+
+
+
 
 def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 'June',
@@ -60,7 +78,7 @@ def clean_date(date_str):
 
     #datetime.date()
 
-def clean_price(price_str, options):
+def clean_price(price_str):
     try:
         price_float = float(price_str)
     except ValueError:
@@ -95,24 +113,44 @@ def clean_id(id_str, options):
                 \r*********************''')
             return
 
+def edit_check(column_name, current_value):
+    print(f'\n**** EDIT {column_name} ****')
+    if column_name == 'Price':
+        print(f'\rCurrent Value: {current_value/100}')
+    elif column_name == 'Date':
+        print(f'\rCurrent Value: {current_value.strftime("%B %d, %Y")}')
+    else:
+        print(f'\rCurrent Value: {current_value}')
+
+    if column_name == 'Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to?')
+            if column_name == 'Date':
+                #Changes are now equal to the clean version of changes
+                changes = clean_date(changes)
+                if type(changes) == datetime.date:
+                    return changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+
+    else:
+        #If not one of these two columns its either author or title - We can just return their reponse as its already in a string format.
+        return input('What would you like to change the value to?')
 
 def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
         for row in data:
             #Only create a book if it doesnt already exist in the database
+            date = clean_date(row[2])
+            price = clean_price(row[3])
             book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
             if book_in_db == None:
-                #print(row)
-                title = row[0]
-                author = row[1]
-                date = clean_date(row[2])
-                price = clean_price(row[3])
                 new_book = Book(title=title, author=author, published_date=date, price=price)
                 session.add(new_book)
-        session.commit()
-
-
+    session.commit()
 
 def app():
     app_running = True
@@ -163,7 +201,22 @@ def app():
                 \n{the_book.title} by {the_book.author}
                 \rPublished: {the_book.published_date}
                 \rPrice: ${the_book.price / 100}''')
-            input('\nPress enter to return to the main menu')
+            sub_choice = submenu()
+            if sub_choice == '1':
+                # edit
+                the_book.title = edit_check('Title', the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.published_date = edit_check('Date', the_book.published_date)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                print('Book updated!')
+                time.sleep(1.5)
+            elif sub_choice == '2':
+                #delete
+                session.delete(the_book)
+                session.commit()
+                print('Book deleted!')
+                time.sleep(1.5)
         elif choice == '4':
             #book analysis
             pass
